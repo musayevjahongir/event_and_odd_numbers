e610086b21934e08826297af2a4bf8bc917b7b3d
musayevjahongir <musayevjahongir5051@gmail.com>
2023-09-22 22:35:57 +0500

    toq raqamlar yig'indisi

--- sum_odd.py
+++ sum_odd.py
@@ -1,6 +1,11 @@
 #A four-digit integer is given. Find the sum of odd digits in it.
 #Create a variable "var_int" and assign it a four-digit integer value.
-
+var_int=int(input("raqamni kiriting: "))
 #Create a variable "sum_odd" and assign it 0.
-
+sum_odd=0
 #Find the sum of the odd digits in the variable "var_int".
+for n in range(4) :
+    if var_int%2==1 :
+        sum_odd+=var_int%10
+    var_int=var_int//10
+print (sum_odd)
