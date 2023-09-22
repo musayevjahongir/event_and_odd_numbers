b27a979f3108ef55069eaf49f2c7b54e00636ff4
musayevjahongir <musayevjahongir5051@gmail.com>
2023-09-22 21:35:57 +0500

    juft bo'lsa 0 chiqadi

--- is_odd.py
+++ is_odd.py
@@ -1,3 +1,7 @@
 #Create a variable "number" and assign it a integer value.
-
+number=int(input("raqamni kiriting: "))
 #Print 1 if the number is odd, otherwise print 0.
+if number%2==0:
+    print(0)
+else:
+    print(1)
