e0ff4b24dc619280142a80960611ebe8f8a7e7dd
musayevjahongir <musayevjahongir5051@gmail.com>
2023-09-22 22:21:42 +0500

    toq sonni aniqlovchi

--- number_odd.py
+++ number_odd.py
@@ -1,4 +1,10 @@
 #A four-digit integer is given. Find the number of odd digits in it.
 #Create a variable "var_int" and assign it a four-digit integer value.
-
+var_int=int(input("raqamni kiriting: "))
 #Print the number of odd digits in the variable "var_int".
+k=0
+for n in range(4) :
+    if var_int%2==1 :
+        k+=1
+    var_int=var_int//10
+print (k)
