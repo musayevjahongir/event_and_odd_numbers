89865df23aba4f4df99858530f759496ba965dc8
musayevjahongir <musayevjahongir5051@gmail.com>
2023-09-24 19:50:40 +0500

    juft raqamlar sonini aniqlovchi

--- number_even.py
+++ number_even.py
@@ -1,10 +1,9 @@
 #A four-digit integer is given. Find the number of even digits in it.
 #Create a variable "var_int" and assign it a four-digit integer value.
-var_int=int(input("raqamni kiriting: "))
+int_var=1964
 #Print the number of even digits in the variable "var_int".
-k=0
-for n in range(4) :
-    if var_int%2==0 :
-        k+=1
-    var_int=var_int//10
-print (k)
+x1=int_var%10
+x2=int_var//10%10
+x3=int_var//100%10
+x4=int_var//1000
+print(x1%2+x2%2+x3%2+x4%2)
