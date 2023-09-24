7011beb20767ec6a2da1966dae02eb32b44a0926
musayevjahongir <musayevjahongir5051@gmail.com>
2023-09-24 19:40:54 +0500

    juft bo'lsa 0 chiqadi

--- is_odd.py
+++ is_odd.py
@@ -1,7 +1,4 @@
 #Create a variable "number" and assign it a integer value.
-number=int(input("raqamni kiriting: "))
+number = 26
 #Print 1 if the number is odd, otherwise print 0.
-if number%2==0:
-    print(0)
-else:
-    print(1)
+print (number % 2)
