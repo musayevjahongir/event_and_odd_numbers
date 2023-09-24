a7f8b70f2fd9163479485383b3d2ee4b52da434f
musayevjahongir <musayevjahongir5051@gmail.com>
2023-09-24 20:45:11 +0500

    juft raqamlar yig'indisini topuvchi

--- sum_even.py
+++ sum_even.py
@@ -8,4 +8,5 @@ x1=var_int%10
 x2=var_int//10%10
 x3=var_int//100%10
 x4=var_int//1000
-print((x1+1)%2*x1+(x2+1)%2*x2+(x3+1)%2*x3+(x4+1)%2*x4)
+sum_even=(x1+1)%2*x1+(x2+1)%2*x2+(x3+1)%2*x3+(x4+1)%2*x4
+print(sum_even)
