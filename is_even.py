56634609055c3f927f4df5bbf599aaf122a4e8f9
musayevjahongir <musayevjahongir5051@gmail.com>
2023-09-24 19:38:14 +0500

    juft bo'lsa 1 chiqaradi

--- is_even.py
+++ is_even.py
@@ -1,7 +1,4 @@
 #Create a variable "number" and assign it a integer value.
-number=int(input("raqamni kiriting: "))
+number = 56
 #Print 1 if the number is even, otherwise print 0.
-if number%2==0:
-    print(1)
-else:
-    print(0)
+print((number+1)%2)
