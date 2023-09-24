c519078cc833b4ad18b1b0737010571dcb299ae9
musayevjahongir <musayevjahongir5051@gmail.com>
2023-09-24 20:42:23 +0500

    juft raqamlar yig'indisini topuvchi

--- sum_even.py
+++ sum_even.py
@@ -1,11 +1,11 @@
 #A four-digit integer is given. Find the sum of even digits in it.
 #Create a variable "var_int" and assign it a four-digit integer value.
-var_int=int(input("raqamni kiriting: "))
+var_int=1226
 #Create a variable "sum_even" and assign it 0.
 sum_even=0
 #Find the sum of the even digits in the variable "var_int".
-for n in range(4) :
-    if var_int%2==0 :
-        sum_even+=var_int%10
-    var_int=var_int//10
-print (sum_even)
+x1=var_int%10
+x2=var_int//10%10
+x3=var_int//100%10
+x4=var_int//1000
+print((x1+1)%2*x1+(x2+1)%2*x2+(x3+1)%2*x3+(x4+1)%2*x4)
